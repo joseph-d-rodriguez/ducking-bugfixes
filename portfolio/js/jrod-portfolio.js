760309324e01b8adb5372e73ef8f2c09cdc989d5
Joseph Rodriguez <josephr@slalom.com>
2015-08-16 23:27:39 -0700

    Adding modals for extra presentation of the HTML, CSS, and JS featured works

--- portfolio/js/jrod-portfolio.js
+++ portfolio/js/jrod-portfolio.js
@@ -1,1 +1,8 @@
 console.log('Loaded jrod-portfolio.js');
+
+var rawHtml = document.getElementsByTagName('html')[0].innerHTML;
+var encodedHtml = rawHtml
+					.replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;')
+					.replace(/"/g, '&quot;');
+document.getElementById('htmlModalContent').innerHTML = encodedHtml;
